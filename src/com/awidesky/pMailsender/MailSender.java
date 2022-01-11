0ba89bc37cf6344a69f43d6c960754af41d391e3
Eugene Hong <awidesky@naver.com>
2022-01-11 12:58:37 +0900

    change file encoding from MS949 to UTF-8

--- src/com/awidesky/pMailsender/MailSender.java
+++ src/com/awidesky/pMailsender/MailSender.java
@@ -86,7 +86,7 @@ public class MailSender {
 		
 		if (files.stream().map(File::length).reduce(0L, (a, b) -> a + b) >= attatchLimit) { //if sum of attachment is bigger than 10MB(probably Naver mail limit)
 			
-			title += " + ¸µÅ©(µé)µµ Å¬¸¯";
+			title += " + ë§í¬(ë“¤)ë„ í´ë¦­";
 			List<File> dropboxed;
 			System.out.println("Mail attachment too big! (>10MB)");
 			System.out.println("Trying dropbox link instead..");
