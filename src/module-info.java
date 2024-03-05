e52085cb18dce828b99139370e6b296a090656d2
Eugene Hong <awidesky@naver.com>
2024-03-05 11:06:43 +0900

    fixing
    https://stackoverflow.com/a/69352490/9287652

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module com.awidesky.pMailSender {
+	requires java.desktop;
+	requires GUIUtil;
+	requires jakarta.activation;
+	requires jakarta.mail;
+	requires dropbox.core.sdk;
+}
