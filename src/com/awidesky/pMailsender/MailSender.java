ab5daa5028ac4ee14ac73004fe9429cb5033faa7
Eugene Hong <awidesky@naver.com>
2026-03-20 21:16:42 +0900

    version 2.0.5

--- src/com/awidesky/pMailsender/MailSender.java
+++ src/com/awidesky/pMailsender/MailSender.java
@@ -52,7 +52,7 @@ import jakarta.mail.internet.MimeUtility;
 
 public class MailSender {
 
-	public static final String VERSION = "v2.0.4";
+	public static final String VERSION = "v2.0.5";
 	public static String projectPath = UserDataPath.appLocalFolder("awidesky", "PMailSender") + File.separator;
 
 	private static LoggerThread loggerThread = new LoggerThread();
